b7588c05ebb952de3b8eddda98da23866e8a8896
Manuel Cosovschi <cosovschim@gmail.com>
2024-06-27 23:06:58 -0300

    Inicio de Desarollo de Login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: [''],
+      email: ['']
+    });
+  }
+
+  onSubmit(): void {
+    console.log('Login exitoso', this.loginForm.value);
+    this.loginForm.reset();
+  }
+}
+
+
 
