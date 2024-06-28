e5f40c6572a0c2a65034d317ce9edaa0d049bef2
Manuel Cosovschi <cosovschim@gmail.com>
2024-06-28 13:55:37 -0300

    Lista de Productos Inicial

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
+  imports: [RouterModule, CommonModule]
 })
-export class ProductListComponent {}
+export class ProductListComponent implements OnInit {
+  products: any[] = [];
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.dataService.getProducts().subscribe(data => {
+      this.products = data;
+    });
+  }
+}
+
+
